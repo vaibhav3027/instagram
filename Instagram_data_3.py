fe38f38bfe5df4a322208e1da20ffe3650b5e720
vaibhav <vaibhav0singhhh@gmail.com>
2020-12-10 20:44:37 +0530

    added login to bypass requirement of login after few scraps

--- Instagram_data_3.py
+++ Instagram_data_3.py
@@ -1,11 +1,10 @@
-import os,time
-import datetime
-import math
-import instaloader
+import os, time, datetime, math, instaloader
 import pandas as pd
+from dotenv import load_dotenv
+
+
 print("Start")
 time.sleep(3)
-#os.system('cmd /k "dir & time"')
 timer = 100
 
 influencer_file = pd.read_excel("Influencer List/Part3_101Influencers.xlsx")
@@ -31,10 +30,7 @@ for profile_id in range(start_id-200,end_id-200):
     print(folder_name)
     if not os.path.exists(folder_name.split("/")[0]):os.mkdir(folder_name.split("/")[0])
 
-    command = f'instaloader --post-filter="date_utc >= {start_date} and date_utc <= {end_date}" {profile_name} --no-metadata-json --no-profile-pic --no-video-thumbnails --dirname-pattern={folder_name} '
-    
-    #command = "dir"
+    command = f'instaloader --post-filter="date_utc >= {start_date} and date_utc <= {end_date}" {profile_name} --no-metadata-json --no-profile-pic --no-video-thumbnails --dirname-pattern={folder_name} --login {os.getenv("INSTAGRAM_USER")} --password {os.getenv("INSTAGRAM_PASSWORD")}'
     os.system(f'cmd /c {command}')              #to close 
-    #command = f'instaloader --post-filter="date_utc >= datetime(2017, 1, 1) and date_utc <= datetime(2018, 3, 30) " justmangobrown --no-metadata-json --no-profile-pic --dirname-pattern={folder_name}'
     
     time.sleep(timer)
